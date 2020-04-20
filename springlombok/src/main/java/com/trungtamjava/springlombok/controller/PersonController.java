d6377008f71b7a176d532ea31c33b2f0d4702831
trungtamjava.com <dinhduccuong1989@gmail.com>
2020-04-20 21:51:29 +0700

    lombok builder

--- springlombok/src/main/java/com/trungtamjava/springlombok/controller/PersonController.java
+++ springlombok/src/main/java/com/trungtamjava/springlombok/controller/PersonController.java
@@ -4,6 +4,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import com.trungtamjava.springlombok.model.Country;
 import com.trungtamjava.springlombok.model.Person;
 
 @Controller
@@ -14,6 +15,20 @@ public class PersonController {
 	public String getPerson(Model model) {
 		Person p = new Person(1, "Java Master");
 		model.addAttribute("p", p);
+		
+		Person p2 = Person.builder().id(2).name("trung tam java").build();
+		
+		model.addAttribute("p2", p2);
+		
+		Person.PersonBuilder pBuilder = p.toBuilder();
+		
+		Person p3 = pBuilder.name("trung tam java 3").build();
+		
+		
+		model.addAttribute("p3", p3);
+		
+		Country vn = Person.builderCountry().id(1).name("VN").build();
+		
 		return "person";
 	}
 }

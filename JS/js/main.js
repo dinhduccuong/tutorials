a38fe5a9fdb1987b4a66379b2a43fef530c3bdf9
trungtamjava.com <dinhduccuong1989@gmail.com>
2020-06-17 00:46:49 +0700

    js tutorials

--- JS/js/main.js
+++ JS/js/main.js
@@ -1,4 +1,29 @@
-var x = 10;
-//alert(x)
-console.log(x)
-document.write(x)
+import { USER_NAME, mangSo } from './user.js'
+
+import * as user from './user.js'
+
+import Person from './user.js'
+
+import { tong as sum } from './user.js'
+
+console.log(USER_NAME)
+console.log(mangSo)
+
+console.log(user.USER_NAME)
+console.log(user.mangSo)
+
+sum(10, 5)
+
+class Student extends Person {
+    constructor(id, name, age) {
+        super(id, name)
+        this._age = age
+    }
+    info() {
+        super.info()
+        console.log(this._age)
+    }
+}
+
+let student = new Student(1, 'A', 20)
+student.info()
